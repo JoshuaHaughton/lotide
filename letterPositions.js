88081f7116697073caae4d51d2e35615d00ef2ee
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-10-27 17:47:12 +0000

    returns the indices that each letter in the given string occurs at

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2) === true ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  return arr1.join(' ') === arr2.join(' ') ? true : false;
+};
+
+
+
+const letterPositions = str => {
+  let out = {};
+  let holder = [];
+  for (let i = 0; i < str.length; i++) {
+    if (!Object.keys(out).includes(str[i]) && str[i] !== ' ') {
+      holder.push(i);
+      out[str[i]] = holder;
+      holder = [];
+    } else if (str[i] === ' ') {
+      continue;
+    } else {
+      out[str[i]].push(i);
+    }
+  }
+  return out;
+};
+
+let expected = {
+  h: [0],
+  e: [1],
+  l: [2, 3],
+  o: [4]
+}
+
+assertArraysEqual(Object.values(letterPositions("hello")), Object.values(expected));
