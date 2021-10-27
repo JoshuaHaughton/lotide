0b11c0436a62e721a4b07b16d488b51412e931a2
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-10-27 19:11:30 +0000

    logs whether or not the 2 given objects have equivalent keys ad values

---
+++ assertObjectsEqual.js
@@ -0,0 +1,28 @@
+const eqObjects = (obj1, obj2) => {
+  let isEqual = true;
+
+  if (Object.keys(obj1).length === Object.keys(obj2).length) { 
+    for (element of Object.keys(obj1)) { 
+      if (!Array.isArray(obj1[element])) {
+        if (obj1[element] !== obj2[element]) {
+          isEqual = false;
+        }
+      } else {
+        if (!eqArrays(obj1[element], obj2[element])) {
+          isEqual = false;
+        }
+      }
+    }
+  } else {
+    isEqual = false;
+  }
+  return isEqual;
+}
+
+const assertObjectsEqual = (obj1, obj2) => {
+  const inspect = require('util').inspect;
+  eqObjects(obj1, obj2) === true ? console.log(`✅✅✅ Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`) : console.log(`🛑🛑🛑 Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
+}
+
+assertObjectsEqual({a: 1}, {a: 1})
+assertObjectsEqual({a: 1}, {a: 2})
