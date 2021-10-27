51059de3c9539dd66a592f3943ac778f69e958db
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-10-27 02:51:51 +0000

    returns an object containing a string that was in both the given object and the given array, with the value of how many times it appeared in the given array.

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  let out = {};
+  for (const a of allItems) {
+    if (!Object.keys(out).includes(a)) {
+      for (const o in itemsToCount) {
+        if (o === a && itemsToCount[o] === true) {
+          out[a] = 1;
+        }
+      }
+    } else {
+      out[a] += 1;
+    }
+  }
+  // console.log(out);
+  return out;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
