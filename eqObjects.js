0a1a25af49d6085bd14914fa56861b32c989d2b7
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-10-27 18:57:16 +0000

    spacing fix

--- eqObjects.js
+++ eqObjects.js
@@ -14,18 +14,13 @@ const eqObjects = (obj1, obj2) => {
   let isEqual = true;
   
   //check if the array which holds the keys of both objects are the same length
-  if (Object.keys(obj1).length === Object.keys(obj2).length) {
-    
-    for (element of Object.keys(obj1)) {
-      
+  if (Object.keys(obj1).length === Object.keys(obj2).length) { 
+    for (element of Object.keys(obj1)) { 
       if (!Array.isArray(obj1[element])) {
-        
         if (obj1[element] !== obj2[element]) {
           isEqual = false;
         }
-      
       } else {
-
         if (!eqArrays(obj1[element], obj2[element])) {
           isEqual = false;
         }
