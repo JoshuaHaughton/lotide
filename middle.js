47abd6474754f968df54c39a0519319c14831c0a
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-10-27 01:50:48 +0000

    returns the middle elements of an array in a new array

---
+++ middle.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2) === true ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  return arr1.join(' ') === arr2.join(' ') ? true : false;
+};
+
+const middle = arr => {
+  let mid = Math.ceil(arr.length/2);
+  let out = [];
+  if (arr.length % 2 !== 0) {
+    out.push(mid)
+  } else {
+    out.push(mid, mid+1)
+  }
+  return out;
+}
+
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5])
