2a559d15b4dde8c07dcc02dd1f87bccb21924aef
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-11-01 02:23:20 +0000

    refactored assertArrayEqual for require and export

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,10 +1,7 @@
-const eqArrays = (arr1, arr2) => {
-  return arr1.join(' ') === arr2.join(' ') ? true : false;
-};
+const eqArrays = require('./eqArray')
 
 const assertArraysEqual = (arr1, arr2) => {
   eqArrays(arr1, arr2) === true ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [12, 3]);
+module.exports = assertArraysEqual;
