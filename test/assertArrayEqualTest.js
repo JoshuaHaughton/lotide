13ca081daad8bcb3d8f3d02fe23908077382d274
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-11-01 02:59:29 +0000

    Refactored our tests using Mocha and Chai

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -3,4 +3,4 @@ const eqArrays = require('../eqArray')
 const assertArraysEqual = require('../assertArrayEqual')
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [12, 3]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
